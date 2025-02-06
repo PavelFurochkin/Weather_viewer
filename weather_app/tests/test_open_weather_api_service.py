6ff52fc5423e15af10764864304984b996c1c1b6
Павел <sunshine4258@gmail.com>
2025-02-06 22:29:17 +0300

    add tests

--- weather_app/tests/test_open_weather_api_service.py
+++ weather_app/tests/test_open_weather_api_service.py
@@ -6,51 +6,12 @@ from django.contrib.messages.storage.fallback import FallbackStorage
 
 from weather_app.dto import LocationWeatherDTO
 from weather_app.models import Location
-from weather_app.views import StartPage
+from weather_app.views import StartPage, SearchPage
 
 User = get_user_model()
 
 
-class LocationResponseMock(MagicMock):
-    status_code = 200
-
-    @staticmethod
-    def json():
-        return [
-            {
-                'name': 'Куево Тутуево',
-                'country': 'RU',
-                'lat': 36.224373,
-                'lon': 117.830135
-            },
-        ]
-
-
-class WeatherResponseMock(MagicMock):
-    status_code = 200
-
-    @staticmethod
-    def json():
-        return {
-            'main': {
-                'temp': 12,
-                'feels_like': 10,
-                'humidity': 60
-            },
-            'weather': [
-                {
-                    'description': 'Гром гремит, земля трясется',
-                    'icon': '02d'
-                }
-            ],
-            'wind': {
-                'speed': 25,
-            }
-
-        }
-
-
-class StartPageTests(TestCase):
+class StartPageTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass333333')
@@ -124,3 +85,66 @@ class StartPageTests(TestCase):
 
         self.assertEqual(response.status_code, 302)
 
+
+class SearchPageTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass333333')
+        self.factory = RequestFactory()
+        self.view = SearchPage()
+
+        self.location1 = Location.objects.create(
+            user=self.user,
+            country='RU',
+            name='Куево Тутуево',
+            latitude=decimal.Decimal('36.2243'),
+            longitude=decimal.Decimal('117.8301')
+        )
+
+        request = self.factory.get('search/', data={'location': self.location1.name})
+        request.user = self.user
+        self.view.request = request
+
+    @patch('weather_app.views.WeatherApiService.get_locations_by_name')
+    def test_search_queryset_success(self, mock_get_location):
+        """
+        Проверяем, что GET-запрос к поисковой странице:
+         - вызывает WeatherApiService.get_locations_by_name с корректными параметрами,
+         - возвращает в контекст список погоды (сформированный на основе результата сервиса).
+        """
+        mock_get_location.return_value = [self.location1.name]
+
+        # Вызываем тестируемый метод get_queryset
+        qs = self.view.get_queryset()
+
+        # Проверяем, что возвращается список из одной записи
+        self.assertEqual(len(qs), 1)
+        # Сравниваем полученное имя локации с ожидаемым
+        self.assertEqual(qs[0], self.location1.name)
+
+        # Проверяем, что метод WeatherApiService.get_locations_by_name вызвался ровно один раз
+        mock_get_location.assert_called_once_with(
+            name=self.location1.name
+        )
+
+    def test_post_save_location(self):
+        data = {
+            'user': 'testuser',
+            'country': 'RU',
+            'name': 'Село Зеленое',
+            'latitude': '36,2245',
+            'longitude': '117,8305'
+        }
+
+        request = self.factory.post('search/', data=data)
+        request.POST = data
+        request.user = self.user
+        request.session = {}
+        # Устанавливаем фейковое хранилище сообщений, чтобы избежать ошибки MessageFailure
+        setattr(request, '_messages', FallbackStorage(request))
+        setattr(self.view, "request", request)
+        response = self.view.post(request)
+
+        # Проверяем, что объект локации был добавлен
+        self.assertTrue(Location.objects.filter(name="Село Зеленое", user=self.user).exists())
+
+        self.assertEqual(response.status_code, 302)
